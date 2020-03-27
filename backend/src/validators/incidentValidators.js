a9ecfb06be9687ed1f16910d2ade412001972360
Bruno Zutim <bzutim@gmail.com>
2020-03-27 16:00:16 -0300

    refactor validators

--- backend/src/validators/incidentValidators.js
+++ backend/src/validators/incidentValidators.js
@@ -1,27 +1,28 @@
 const { celebrate, Segments, Joi } = require('celebrate');
 
-const index = celebrate({
-    [Segments.QUERY]: Joi.object().keys({
-        page: Joi.number()
-    })
-});
+module.exports = {
 
-const store = celebrate({
-    [Segments.HEADERS]: Joi.object({
-        authorization: Joi.string().required().length(8)
-    })
-    .unknown(),
-    [Segments.BODY]: Joi.object().keys({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        value: Joi.number().required(),
-    })
-});
+    index: celebrate({
+       [Segments.QUERY]: Joi.object().keys({
+           page: Joi.number()
+       })
+    }),
+    
+    store: celebrate({
+       [Segments.HEADERS]: Joi.object({
+           authorization: Joi.string().required().length(8)
+       })
+       .unknown(),
+       [Segments.BODY]: Joi.object().keys({
+           title: Joi.string().required(),
+           description: Joi.string().required(),
+           value: Joi.number().required(),
+       })
+    }),
 
-const destroy = celebrate({
-    [Segments.PARAMS]: Joi.object().keys({
-        id: Joi.number().required()
-    })
-});
-
-module.exports = { index, store, destroy };
+    destroy: celebrate({
+       [Segments.PARAMS]: Joi.object().keys({
+           id: Joi.number().required()
+       })
+    }),
+}
--- backend/src/validators/profileValidators.js
+++ backend/src/validators/profileValidators.js
@@ -1,10 +1,12 @@
 const { celebrate, Segments, Joi } = require('celebrate');
 
-const index = celebrate({
-    [Segments.HEADERS]: Joi.object({
-        authorization: Joi.string().required().length(8)
-    })
-    .unknown()
-});
 
-module.exports = { index };
+module.exports = { 
+    
+    index: celebrate({
+        [Segments.HEADERS]: Joi.object({
+            authorization: Joi.string().required().length(8)
+        })
+        .unknown()
+    }),
+ };
